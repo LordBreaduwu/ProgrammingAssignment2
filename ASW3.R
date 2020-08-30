efef681315429e1a100730c4a31f33aba6f3aeb1
LordBreaduwu <68828992+LordBreaduwu@users.noreply.github.com>
2020-08-30 11:25:58 -0500

    Add files via upload

---
+++ ASW3.R
@@ -0,0 +1,34 @@
+## we have two functions, makecahematrix that consists 
+## of set, get, setinv, getinv and then we have makecachematrix
+## to calculate the inverse for non squared i use the solve function 
+
+library(MASS)
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULLcache
+  }
+  get <- function()x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function(){
+                          inver<-ginv(x)
+                          inver%*%x
+                          }
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting the cached data, please wait")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(data,...)
+  x$setInverse(inv)
+  inv
+}
+
